dfc41d106c6c06ddb35f0bf1828794746fdee7c7
Kuree <xmlc001@gmail.com>
2014-09-27 09:11:12 -0400

    Add helper form and export function.

---
+++ DSTResourceHelper/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2014 by Keyi Zhang                                       *
+ *  kz005@bucknell.edu                                                     *
+ *                                                                         *
+ *  This file is part of the Sims 4 Package Interface (s4pi)               *
+ *                                                                         *
+ *  s4pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s4pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+namespace DSTResourceHelper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Location = new System.Drawing.Point(13, 13);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(680, 512);
+            this.panel.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(618, 552);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.Location = new System.Drawing.Point(537, 552);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(456, 552);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 587);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.panel);
+            this.Name = "MainForm";
+            this.Text = "DST Resource Helper";
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ DSTResourceHelper/MainForm.cs
@@ -0,0 +1,121 @@
+﻿/***************************************************************************
+ *  Copyright (C) 2014 by Keyi Zhang                                       *
+ *  kz005@bucknell.edu                                                     *
+ *                                                                         *
+ *  This file is part of the Sims 4 Package Interface (s4pi)               *
+ *                                                                         *
+ *  s4pi is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  s3pi is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with s4pi.  If not, see <http://www.gnu.org/licenses/>.          *
+ ***************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using s4pi.Helpers;
+using s4pi.ImageResource;
+using s4pi.Interfaces;
+using System.IO;
+using s4pi.ImageResource;
+
+namespace DSTResourceHelper
+{
+    public partial class MainForm : Form
+    {
+        private string[] args;
+        private FileStream fs;
+        DSTResource dst;
+        static bool channel1 = true, channel2 = true, channel3 = true, channel4 = true, invertch4 = false;
+        DDSPanel ddsPanel;
+
+        public MainForm(string[] args)
+        {
+            InitializeComponent();
+            this.args = args;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            if (this.args == null || this.args.Length < 2)
+                this.Close();
+            string path = this.args[1];
+            if (!File.Exists(path)) this.Close();
+            fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            dst = new s4pi.ImageResource.DSTResource(1, fs);
+            ddsPanel = new DDSPanel()
+            {
+                Fit = true,
+                Channel1 = channel1,
+                Channel2 = channel2,
+                Channel3 = channel3,
+                Channel4 = channel4,
+                InvertCh4 = invertch4,
+                Margin = new Padding(3),
+            };
+            ddsPanel.Channel1Changed += (sn, e2) => channel1 = ddsPanel.Channel1;
+            ddsPanel.Channel2Changed += (sn, e2) => channel2 = ddsPanel.Channel2;
+            ddsPanel.Channel3Changed += (sn, e2) => channel3 = ddsPanel.Channel3;
+            ddsPanel.Channel4Changed += (sn, e2) => channel4 = ddsPanel.Channel4;
+            ddsPanel.InvertCh4Changed += (sn, e2) => invertch4 = ddsPanel.InvertCh4;
+            
+            this.panel.Controls.Add(ddsPanel);
+            ddsPanel.DSTLoad(dst.Stream, false);
+            ddsPanel.Width = dst.Width;
+            ddsPanel.Height = dst.Height;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (dst == null) return;
+            using (SaveFileDialog save = new SaveFileDialog()
+            {
+                Filter = "DXT Image|*.dds"
+            })
+            {
+                if(save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    using(FileStream fs2 = new FileStream(save.FileName, FileMode.Create, FileAccess.Write))
+                    {
+                        dst.ToDDS().CopyTo(fs2);
+                    }
+                }
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (dst == null) return;
+            using (OpenFileDialog open = new OpenFileDialog()
+            {
+                Filter = "DXT Image|*.dds"
+            })
+            {
+                if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    using (FileStream fs2 = new FileStream(open.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        this.dst.ImportToDST(fs2);
+                        ddsPanel.DSTLoad(dst.Stream, false);
+                        ddsPanel.Width = dst.Width;
+                        ddsPanel.Height = dst.Height;
+                    }
+                }
+            }
+        }
+
+    }
+}
